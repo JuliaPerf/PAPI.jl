fd32f0ab68ddbfeaa32d99324cd772e06b32e834
Tom Haber <tom.haber@uhasselt.be>
2020-11-13 22:00:21 +0100

    split events into groups

--- src/eventsets.jl
+++ src/eventsets.jl
@@ -11,5 +11,32 @@ function destroy_eventset(evtset::EventSet)
     @papichk ccall((:PAPI_destroy_eventset, :libpapi), Cint, (Ptr{Cint},), evtset)
 end
 
-add_event(evtset::EventSet, evt::Event) = @papichk ccall((:PAPI_add_event, :libpapi), Cint, (Cint, Cuint), evtset[], evt)
+add_event(evtset::EventSet, evt::Event) = ccall((:PAPI_add_event, :libpapi), Cint, (Cint, Cuint), evtset[], evt) == PAPI_OK
 remove_event(evtset::EventSet, evt::Event) = @papichk ccall((:PAPI_remove_event, :libpapi), Cint, (Cint, Cuint), evtset[], evt)
+
+function find_group(evt::Event, groups::Vector{EventSet})
+    for (j, g) in enumerate(groups)
+        if add_event(g, evt)
+            return j
+        end
+    end
+
+    push!(groups, create_eventset())
+    return length(groups)
+end
+
+function measurement_groups(events::Vector{Event})
+    groups = EventSet[]
+    assignment = zeros(Int64, size(events))
+
+    @inbounds for (i, evt) in enumerate(events)
+        j = find_group(evt, groups)
+        assignment[i] = j
+    end
+
+    @inbounds for j in 1:length(groups)
+        destroy_eventset(groups[j])
+    end
+
+    assignment
+end
