4faa41bb9403883f043062792fe4db4467568add
Tom Haber <tom.haber@uhasselt.be>
2020-11-14 14:24:58 +0100

    docs for sample and profile

--- src/sampling.jl
+++ src/sampling.jl
@@ -18,7 +18,23 @@ EventStats(events::Vector{Event}) = EventStats(events, zeros(Counts, Counts[]), 
 
 gcscrub() = (GC.gc(); GC.gc(); GC.gc(); GC.gc())
 
-function profile(f::Function, events::Vector{Event}; gcfirst::Bool=false, warmup::Int64=1)
+"""
+    profile(f::Function, events::Vector{Event}; gcfirst::Bool=true, warmup::Int64=0)
+
+Execute the function `f` once while counting specific `events`.
+
+**Arguments**:
+
+    -`f`: the function to profile
+    -`events`: the events to count
+    -`gcfirst`: run the gc several times before the execution to reduce gc noise
+    -`warmup`: number of times to run the function prior to counting
+
+**Return values**:
+
+`EventValues` containing the events, counts and runtime collected
+"""
+function profile(f::Function, events::Vector{Event}; gcfirst::Bool=true, warmup::Int64=0)
     gcfirst && gcscrub()
 
     for i in 1:warmup
@@ -40,7 +56,25 @@ end
 
 profile(f::Function, events::NTuple{N, Event}) where N = profile(f, collect(events))
 
-function sample(f::Function, events::Vector{Event}; max_secs::Int64=5, max_epochs::Int64=1000, gcsample::Bool=false, warmup::Int64=1)
+"""
+    sample(f::Function, events::Vector{Event}; max_secs::Float64=5, max_epochs::Int64=1000, gcsample::Bool=false, warmup::Int64=1)
+
+Execute the function `f` several times, each time counting specific `events`.
+Sampling continues until either the maximum number of samples `max_epochs` are collected or the runtime budget `max_secs` is exceeded.
+
+**Arguments**:
+
+    -`f`: the function to profile
+    -`events`: the events to count
+    -`max_secs`:
+    -`gcsample`: run the gc several times before the execution to reduce gc noise
+    -`warmup`: number of times to run the function prior to counting
+
+**Return values**:
+
+`EventStats` containing the events, counts and runtime collected
+"""
+function sample(f::Function, events::Vector{Event}; max_secs::Float64=5., max_epochs::Int64=1000, gcsample::Bool=false, warmup::Int64=1)
     num_events = length(events)
     counts = Vector{Counts}(undef, num_events)
     samples = Vector{Counts}[]
@@ -91,6 +125,20 @@ function kwargs(default_events, ex, args...)
     events, ex, params
 end
 
+"""
+    profile(ex, args...)
+
+Convience macro for profiling an expression.
+Events can be specified as a first argument, otherwise the default events `[BR_INS, BR_MSP, TOT_INS, TOT_CYC]` are counted
+
+Arguments and return values are similar to [`profile`](@ref)
+
+**Example**:
+```julia
+@profile f(x, y, z) # sampling default events
+@profile [PAPI.TOT_INS, PAPI.DP_OPS, native_event] f(x, y, z) gcfirst=false
+```
+"""
 macro profile(ex, args...)
     events, ex, params = kwargs([BR_INS, BR_MSP, TOT_INS, TOT_CYC], ex, args...)
     quote
@@ -98,6 +146,20 @@ macro profile(ex, args...)
     end
 end
 
+"""
+    sample(ex, args...)
+
+Convience macro for sampling an expression.
+Events can be specified as a first argument, otherwise the default events `[BR_INS, BR_MSP, TOT_INS, TOT_CYC]` are counted
+
+Arguments and return values are similar to [`sample`](@ref)
+
+**Example**:
+```julia
+@sample f(x, y, z) # sampling default events
+@sample [PAPI.TOT_INS, PAPI.DP_OPS, native_event] f(x, y, z) max_secs=1
+```
+"""
 macro sample(ex, args...)
     events, ex, params = kwargs([BR_INS, BR_MSP, TOT_INS, TOT_CYC], ex, args...)
     quote
