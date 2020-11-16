9757b76002204f31a6e7ec27f1ad76f5de3c82d5
Tom Haber <tom.haber@uhasselt.be>
2020-11-16 16:34:08 +0100

    available_native

--- src/PAPI.jl
+++ src/PAPI.jl
@@ -45,5 +45,5 @@ end
 
 export PAPIError, num_counters, start_counters, read_counters!, accum_counters!, stop_counters, stop_counters!
 export @profile, @sample, sample, profile, name_to_event, event_to_name, @event_str
-export find_component, exists, available_presets, Event, Counts, EventSet
+export find_component, exists, available_presets, available_native, Event, Counts, EventSet
 end # module
