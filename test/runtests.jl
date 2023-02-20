3f15f4498293322a605233a9c1aa70767364b899
Valentin Churavy <v.churavy@gmail.com>
2023-02-20 18:50:37 -0500

    fixup! rework initialization

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using PAPI, Test
 
+if !PAPI.is_available()
+    @info "PAPI is not available on this Platform use PAPI.set_library"
+    exit()
+end
+
 include("eventset.jl")
 #include("counting.jl")
 include("serialization.jl")
